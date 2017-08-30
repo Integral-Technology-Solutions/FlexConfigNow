e003ead9f9d9ca363559ce5e36add783cccae41a
Matt Spencer <matthew_spencer_1994@hotmail.com>
2017-08-30 10:03:51 +1000

    Implementation of all classes outlined in confignow documentation. No test cases written for most.

---
+++ FlexDeployConfigNowPlugin-1.0.1/src/plugin/confignow/operations/stopWlsAdmin.java
@@ -0,0 +1,52 @@
+package plugin.confignow.operations;
+
+import flexagon.fd.core.plugin.AbstractPluginProvider;
+import flexagon.fd.core.plugin.PluginResult;
+import flexagon.fd.core.workflow.WorkflowExecutionContext;
+import flexagon.ff.common.core.logging.FlexLogger;
+import plugin.confignow.configNowProperties;
+import plugin.confignow.executionHelper;
+import plugin.confignow.validationHelper;
+
+/**
+ * Created by Matt on 30/08/2017.
+ */
+public class stopWlsAdmin extends AbstractPluginProvider {
+
+    private static final String CLZ_NAM = stopWlsAdmin.class.getName();
+    private static final FlexLogger LOG = FlexLogger.getLogger(CLZ_NAM);
+
+    private String mEnvironment;
+    private String mConfigFile;
+
+    /* Execute the plugin operation */
+    @Override
+    public PluginResult execute(){
+        String method = "execute";
+        LOG.logInfoEntering(method);
+
+        WorkflowExecutionContext context = getWorkflowExecutionContext();
+        String command = "stop_wls_admin";
+
+        executionHelper.executeCommand(context, command, mEnvironment, mConfigFile);
+
+        LOG.logInfoExiting(method);
+        return PluginResult.createPluginResult(context);
+    }
+
+    /* Validate that environment code has a folder which contains config file */
+    @Override
+    public void validate(){
+        String method = "validate";
+        LOG.logInfoEntering(method);
+
+        WorkflowExecutionContext workflowExecutionContext = getWorkflowExecutionContext();
+        String configFile = getStringInput(configNowProperties.FDCN_CONFIG_FILE);
+        if(validationHelper.validateInputs(workflowExecutionContext, LOG, configFile)){
+            this.mConfigFile = configFile;
+            this.mEnvironment = workflowExecutionContext.getEnvironment().getCode();
+        }
+
+        LOG.logInfoExiting(method);
+    }
+}
